9c6a9a2cb49199f9e36a1f81137cb42a3b31623e
Liana Manucharyan <liana.manucharyan002@gmail.com>
2024-09-05 18:23:51 -0300

    Estilización y arreglo de errores

--- JS/login.js
+++ JS/login.js
@@ -1,24 +1,16 @@
-// Selecciona el formulario
 const loginForm = document.querySelector('#loginForm');
 
-// Verifica si el formulario existe
 if (loginForm) {
     loginForm.addEventListener('submit', (e) => {
         e.preventDefault();
 
         try {
-            // Obtén los valores de los campos
             const email = document.querySelector('#email').value;
             const password = document.querySelector('#password').value;
-
-            // Obtén los usuarios almacenados
-            const Users = JSON.parse(localStorage.getItem('users')) || [];
-            
-            // Busca un usuario válido
-            const validUser = Users.find(user => user.email === email && user.password === password);
+            const users = JSON.parse(localStorage.getItem('users')) || [];
+            const validUser = users.find(user => user.email === email && user.password === password);
 
             if (!validUser) {
-                // Muestra un mensaje si el usuario no es válido
                 Swal.fire({
                     title: '¡Lo sentimos!',
                     text: 'Usuario y/o contraseña incorrectos',
@@ -29,7 +21,6 @@ if (loginForm) {
                 return;
             }
 
-            // Muestra un mensaje de bienvenida si el usuario es válido
             Swal.fire({
                 text: '¡Bienvenid@ a Joyería Ian!',
                 icon: 'success',
@@ -37,15 +28,12 @@ if (loginForm) {
                 confirmButtonText: 'Ingresar'
             }).then((result) => {
                 if (result.isConfirmed) {
-                    // Redirige y almacena el éxito del login
                     window.location.href = '../index.html';
                     localStorage.setItem('login_success', JSON.stringify(validUser));
                 }
             });
 
         } catch (error) {
-            // Muestra un mensaje de error en caso de excepción
-            console.error('Error during login process:', error);
             Swal.fire({
                 title: 'Error',
                 text: 'Ocurrió un problema inesperado. Por favor, intente nuevamente.',
@@ -53,6 +41,14 @@ if (loginForm) {
                 iconColor: 'red',
                 confirmButtonText: 'Aceptar'
             });
-        } 
         }
-    )};
+    });
+} else {
+    Swal.fire({
+        title: 'Error',
+        text: 'El formulario de inicio de sesión no se encuentra.',
+        icon: 'error',
+        iconColor: 'red',
+        confirmButtonText: 'Aceptar'
+    });
+}
